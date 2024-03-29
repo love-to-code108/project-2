07b077224ead7139e5f2bbc5f434cdec75720f51
love.to.code108 <love.to.code108@gmail.com>
2024-03-29 22:50:05 +0530

    final commit for the day

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,37 @@ function App() {
 
   return (
     <div>
-      Hello World
-      <InputFieldAndCurrencySelector fromTo={"From"}/>
-      <InputFieldAndCurrencySelector fromTo={"To"}/>
+      <Mainapp/>
     </div>
   )
 }
 
 export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function Mainapp() {
+  
+
+  return (
+    <div className=' bg-red-600 w-[100%] h-[100vh] flex justify-center items-center'>
+      <div>
+      <InputFieldAndCurrencySelector fromTo={"From"}/>
+      <InputFieldAndCurrencySelector fromTo={"To"}/>
+      </div>
+
+    </div>
+  )
+}
